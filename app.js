251f07fbe7c822c41bd5fadd1ef469628c14af84
Eñaut Agirre <enaut.irakasle@gmail.com>
2019-06-04 23:35:32 +0200

    animazioa ok

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-    $(".card a").click(function(event){
+$(document).ready(function () {
+    $(".card a").click(function (event) {
         //var pos = $('#cantidad').position();
-       // alert(pos.top);
-       var clicado = $(this).parents('.card');
-       //cardaren momentuko zaballuze eta posizioa jaso
+        // alert(pos.top);
+        var card_clicado = $(this).parents('.card');
+        //cardaren momentuko zaballuze eta posizioa jaso
+        var ancho = card_clicado.width();
+        var alto = card_clicado.height();
+        var posicion_card = card_clicado.position();
 
-       //ezarri cardari
-       //position fixed
-       //z-index 100 jarri navaren aurretik joateko
+        //ezarri cardari
 
-       //carrito botoiarenak jaso
+        //position fixed
+        card_clicado.css('position', 'fixed');
+        card_clicado.width(ancho);
+        card_clicado.height(alto);
+        //z-index 100 jarri navaren aurretik joateko
 
-        $(this).parents('.card').animate({
-                position:'abosolute',
-                opacity: 0.4,
-                borderColor: "red",
-                borderWidth: "10px",
-                left: '+=50px'
-                //left: $('#cantidad').position().left,
-                //top: $('#cantidad').position().top
-              }, 5500
-        );
+        //carrito botoiaren posizioa jaso
+        var posicion_carrito = $('#cantidad').position();
+
+        card_clicado.animate({
+            opacity: 0.1,
+            left: posicion_carrito.left,
+            top: posicion_carrito.top,
+            width: 1,
+            height: 1,
+            borderRadius: 50
+        }, 1000);
+        card_clicado.hide('slow');
     });
 });
